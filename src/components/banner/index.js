abbe6b7af8f4f31e177149fd79d90798203589d9
nhanlnt <nhanlnt@smartosc.com>
2022-08-21 08:42:37 +0700

    ci: update site

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -105,45 +105,46 @@ const Banner = () => {
             )}
           </div>
 
-          <div className="w-full flex flex-col justify-center items-center space-y-4 md:w-6/12 z-10 order-1 p-2 md:order-2 banner-right__side">
-            <div className="banner-right__side-container">
-              <div className="w-full relative banner-right__side-wrapper">
-                <img
-                  src={BannerImage}
-                  alt="Suky Learn to Fly"
-                  className="absolute w-full h-full left-0 top-0 object-contain"
-                />
+          <div className="w-full h-max flex flex-col justify-center items-center space-y-4 md:w-6/12 z-10 order-1 p-2 md:order-2 relative banner-right__side">
+            <div className="blur-bg"/>
+              <div className="banner-right__side-container">
+                <div className="w-full relative banner-right__side-wrapper">
+                  <img
+                    src={BannerImage}
+                    alt="Suky Learn to Fly"
+                    className="absolute w-full h-full left-0 top-0 object-contain"
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="flex flex-col justify-center items-center">
-              <h1 className="text-2xl text-white font-bold text-center lg:text-4xl xl:text-6xl">
-                Together, let's
-                <span className="font-semibold text-pink"> fly</span>
-              </h1>
-              <h2 className="text-white text-center text-base lg:text-lg xl:text-2xl mt-3">
-                Suki invites you to join her on a world of adventures. 5000
-                unique characters inspired by fantasy and slice-of-life anime.
-                Come with us on this journey?
-              </h2>
-            </div>
+              <div className="flex flex-col justify-center items-center z-10">
+                <h1 className="text-2xl text-white font-bold text-center lg:text-4xl xl:text-6xl">
+                  Together, let's
+                  <span className="font-semibold text-pink"> fly</span>
+                </h1>
+                <h2 className="text-white text-center text-base lg:text-lg xl:text-2xl mt-3">
+                  Suki invites you to join her on a world of adventures. 5000
+                  unique characters inspired by fantasy and slice-of-life anime.
+                  Come with us on this journey?
+                </h2>
+              </div>
 
-            <div className="w-full flex justify-center items-center">
-              {(!blockchain.account || !blockchain.smartContract) && (
-                <Button
-                  label={"Mint your Suki(s)"}
-                  action={onConnectWalletBtnClick}
-                />
-              )}
-            </div>
+              <div className="w-full flex justify-center items-center">
+                {(!blockchain.account || !blockchain.smartContract) && (
+                  <Button
+                    label={"Mint your Suki(s)"}
+                    action={onConnectWalletBtnClick}
+                  />
+                )}
+              </div>
 
-            <div className="w-full">
-              <CountDown target={"Aug 21, 2022 18:00:00"} />
-            </div>
+              <div className="w-full z-10">
+                <CountDown target={"Aug 21, 2022 18:00:00"} />
+              </div>
 
-            <div className="w-full flex justify-center items-center">
-              <Social />
-            </div>
+              <div className="w-full flex justify-center items-center z-10">
+                <Social />
+              </div>
           </div>
         </div>
       </div>
